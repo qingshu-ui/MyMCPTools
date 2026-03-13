27f946b931664cde80fa7f72ac42814f2e6e251c
qingshu-ui <81049953+qingshu-ui@users.noreply.github.com>
2026-03-13 20:12:46 +0800

    initial project

---
+++ mcp-audio-tools/build.gradle.kts
@@ -0,0 +1,90 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink
+import org.jetbrains.kotlin.konan.target.HostManager
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinxSerialization)
+}
+
+group = "io.github.qingshu-ui"
+version = "1.0.0"
+
+kotlin {
+    jvmToolchain(21)
+    jvm {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    listOf(
+        mingwX64(),
+        linuxX64(),
+        linuxArm64(),
+    ).forEach { target ->
+        target.apply {
+            binaries {
+                executable {
+                    entryPoint = "io.github.qingshu.mcpaudiotools.main"
+                    baseName = "-$version"
+                }
+            }
+        }
+    }
+
+    compilerOptions {
+        freeCompilerArgs.addAll(
+            "-Xexpect-actual-classes",
+        )
+    }
+
+    applyDefaultHierarchyTemplate()
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines)
+            implementation(libs.kotlinx.serializationJson)
+            implementation(libs.modelcontextprotocol.kotlinSdk)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+tasks.register<Jar>("fatJar") {
+    group = "build"
+    archiveClassifier.set("all")
+
+    from(kotlin.targets["jvm"].compilations["main"].output)
+    dependsOn(configurations["jvmRuntimeClasspath"])
+
+    from({
+        configurations["jvmRuntimeClasspath"]
+            .filter { it.name.endsWith(".jar") }
+            .map { zipTree(it) }
+    })
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    manifest {
+        attributes(
+            "Main-Class" to "io.github.qingshu.mcpaudiotools.MainKt",
+        )
+    }
+}
+
+tasks.withType<KotlinNativeLink>().configureEach {
+    val taskName = name.lowercase()
+
+    val shouldBeEnabled =
+        when {
+            taskName.contains("linux") -> HostManager.hostIsLinux
+            taskName.contains("mingw") -> HostManager.hostIsMingw
+            taskName.contains("macos") -> HostManager.hostIsMac
+            else -> true
+        }
+
+    enabled = shouldBeEnabled
+}
---
+++ mcp-audio-tools/src/commonMain/kotlin/Main.kt
@@ -0,0 +1,4 @@
+package io.github.qingshu.mcpaudiotools
+
+fun main(args: Array<String>) {
+}
