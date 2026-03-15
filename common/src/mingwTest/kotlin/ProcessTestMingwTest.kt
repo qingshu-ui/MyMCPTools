d2c7ca09d1757daa300eb91d98e503baeba1f212
qingshu-ui <81049953+qingshu-ui@users.noreply.github.com>
2026-03-16 00:38:02 +0800

    Add process test for setting environment variable in mingwTest

---
+++ common/src/mingwTest/kotlin/ProcessTestMingwTest.kt
@@ -0,0 +1,20 @@
+package io.github.qingshu.mcptool.common
+
+import kotlinx.coroutines.runBlocking
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ProcessTestMingwTest {
+
+    @Test
+    fun `test set environment variable`(): Unit = runBlocking {
+        val binPath = "d:\\Users\\17186\\Downloads\\Compressed\\subtitle-to-lrc_windows-amd64\\subtitle-to-lrc.exe"
+        val process = ProcessBuilder("cmd", "/c", "%SUBTITLE_TO_LRC%", "-h")
+            .environment("SUBTITLE_TO_LRC", binPath)
+            .mergeStderr(true)
+            .start()
+
+        process.stdoutLines().collect(::println)
+        assertEquals(0, process.waitFor())
+    }
+}
